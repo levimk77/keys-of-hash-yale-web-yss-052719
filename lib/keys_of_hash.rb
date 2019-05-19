913d8b0bd8c98827bf24c2985be5146632853034
Elijah Florence <efloren@siue.edu>
2019-05-19 22:02:36 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,10 @@
 class Hash
   def keys_of(arguments)
+    array = []
     arguments.each do |argument|
-      if argument.
+      if argument.Class == Hash
+        self.each do |key, value|
+          array << key
+      end
   end
 end
